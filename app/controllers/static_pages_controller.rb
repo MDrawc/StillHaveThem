fc3b243fe502cd6120ad75906f1678e0be15c0dc
MDrawc <pistolisloaded@gmail.com>
2019-04-30 21:49:05 +0200

    Add basic search by name of the game.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,25 @@ class StaticPagesController < ApplicationController
   end
 
   def search
+    if (inquiry = params[:inquiry])
 
+      api_endpoint = 'https://api-v3.igdb.com/games'
+      request_headers = { headers: { 'user-key' => '605c81028ed48d0b6ee68e29dd247b75' } }
+
+      api = Apicalypse.new(api_endpoint, request_headers)
+      api
+        .fields(:name, :first_release_date)
+        .search(params[:inquiry])
+        .limit(50)
+        .offset(0)
+
+      @search_results = api.request
+      @search_results.map do |game|
+        (release_date = game["first_release_date"]) ?
+        game["first_release_date"] = Time.at(1317686400).year
+         : game["first_release_date"] = "unknown"
+     end
+    end
   end
 
 end
