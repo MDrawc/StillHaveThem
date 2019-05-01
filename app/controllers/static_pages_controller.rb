42c380867686a780d9cfd522b98f4f774d66391f
MDrawc <pistolisloaded@gmail.com>
2019-05-02 00:28:06 +0200

    Move search feature form controller to model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,28 +7,8 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    if inquiry = params[:inquiry]
-
-      http = Net::HTTP.new('api-v3.igdb.com', 80)
-      request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/games'), {'user-key' => ENV['IGDB_KEY'] })
-
-      # 1:
-      request.body = "fields name, first_release_date; search #{inquiry.inspect}; limit 50;"
-      first_bucket = JSON.parse http.request(request).body
-      first_size = first_bucket.size
-
-      # 2:
-      request.body  = "fields name, first_release_date; where name ~ #{inquiry.inspect}*; sort popularity desc; limit 50;"
-      second_bucket = JSON.parse http.request(request).body
-      second_size = second_bucket.size
-
-      # 1+2:`
-      sum_bucket = first_bucket + second_bucket
-      sum_bucket.uniq!
-      sum_size = sum_bucket.size
-
-      @search_results = sum_bucket
-
+    if params[:inquiry]
+      @search_results = GameQuery.new(params[:inquiry]).results
       respond_to do |format|
         format.js
       end
