66f779af0455e83cf6a776161a2fec08c95e5b78
Michal Drawc <pistolisloaded@gmail.com>
2019-06-03 14:03:22 +0200

    Delete old create user form files. Add log in form. Style log in form. Create sessions controller.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
