bdbc6124e9a1e3c0ca07d73d40305619ad9ace9b
MDrawc <pistolisloaded@gmail.com>
2019-06-07 18:41:04 +0200

    Add some styling to game info. Add slideshow to game info.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -5,16 +5,72 @@ module StaticPagesHelper
   def get_game_cover(game, options = { width: 200 })
     cover = game["cover"]
     if cover.nil? || cover.class != Hash
-      no_cover = content_tag(:div, content_tag(:p, game["name"] ,
-        class: "no-cover-tag unselectable"), class: "no-cover")
-      return no_cover
+      render partial: "no_cover", locals: { game: game }
+    else
+      cover_id = cover["image_id"]
+      cover_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/'
+      cover_url += cover_id.to_s + ".jpg"
+      render partial: "cover", locals: { game: game, cover_url: cover_url,
+       options: options }
     end
+  end
+
+  def get_cover_url(game)
+    cover = game["cover"]
+    return nil if cover.nil? || cover.class != Hash
     cover_id = cover["image_id"]
     cover_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/'
     cover_url += cover_id.to_s + ".jpg"
+  end
 
-    render partial: "cover", locals: { game: game, cover_url: cover_url,
-     options: options }
+  def get_summary(game, f_max, l_min)
+    if game['summary']
+      first, counter = [], 0
+      words = game['summary'].scan(/[\S]+|\s/)
+
+      while counter < f_max && !words.empty? do
+        first << last = words.shift
+        counter += last.size
+      end
+
+      if words.join.size < l_min
+        first += words
+        first = first.join.strip
+        words, span = nil
+      else
+        first = first.join.strip
+        span = first.last.scan(/[\.]/).present? ? '..' : '...'
+        words = words.join.strip
+      end
+
+      return { start: first, span: span, end: words }
+    else
+      return false
+    end
+  end
+
+  def get_screenshots(game)
+    screenshots = game['screenshots']
+    urls = []
+    if screenshots && screenshots.class == Array
+      if screenshots.any? { |thing| thing.class == Hash }
+
+        filtered = screenshots.map { |thing| thing if thing.class == Hash }
+        filtered = filtered.compact
+
+        filtered.each do |screen|
+          base_url = 'https://images.igdb.com/igdb/image/upload/'
+          url_big = base_url + 't_original/' + screen['image_id'] + '.jpg'
+          url_small = url_big.sub('original', 'screenshot_med')
+          urls << {big: url_big, small: url_small}
+        end
+        return urls
+      else
+        return false
+      end
+    else
+      return false
+    end
   end
 
   def get_api_status
