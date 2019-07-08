88078b0e73e48e3d8b6f58371ab3fd76a72f0133
Michal Drawc <pistolisloaded@gmail.com>
2019-07-08 15:50:16 +0200

    Add cover made of screenshot. Better styled no cover.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,30 +2,24 @@ require 'net/https'
 
 module StaticPagesHelper
 
-  def get_game_cover(game, options = { width: 200 })
+  def get_game_cover(game, width = 200)
     cover = game["cover"]
     if cover.nil? || cover.class != Hash
-      render partial: "cover_view/no_cover", locals: { game: game }
+      screenshots = game['screenshots']
+      if screenshots && screenshots.class == Array
+        if screenshots.first.class == Hash
+          base_url = 'https://images.igdb.com/igdb/image/upload/'
+          url = base_url + 't_screenshot_med/' + screenshots.first['image_id'] + '.jpg'
+          return { url: url, width: width, height: 266, from_scrn: 's-cover' }
+        end
+      end
     else
-      cover_id = cover["image_id"]
       ratio = cover["height"]/cover["width"].to_f
-
-      width = options[:width]
       height = (width * ratio).round(2)
-
       cover_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/'
-      cover_url += cover_id.to_s + ".jpg"
-      render partial: "cover_view/cover", locals: { game: game, cover_url: cover_url,
-        width: width, height: height, options: options }
+      cover_url += cover["image_id"].to_s + ".jpg"
+      return { url: cover_url, width: width, height: height }
     end
-  end
-
-  def get_cover_url(game)
-    cover = game["cover"]
-    return nil if cover.nil? || cover.class != Hash
-    cover_id = cover["image_id"]
-    cover_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/'
-    cover_url += cover_id.to_s + ".jpg"
   end
 
   def get_summary(game, f_max, l_min)
@@ -94,32 +88,6 @@ module StaticPagesHelper
     end
   end
 
-  def get_api_status
-
-    http = Net::HTTP.new('api-v3.igdb.com', 80)
-    request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/api_status'),
-     { 'user-key' => ENV['IGDB_KEY'] })
-    request.body = "fields *;"
-
-    begin
-      data = JSON.parse http.request(request).body
-      data = data[0]["usage_reports"]["usage_report"]
-      current = data["current_value"]
-      max = data["max_value"]
-      to_next_period = data["period_end"].to_date
-      days_left = (to_next_period - Date.today).to_i
-      requests_left = max - current
-      status = requests_left > 0
-
-      rescue JSON::ParserError
-        days_left = "???"
-        requests_left = "???"
-        status = nil
-    end
-
-    [status, requests_left, days_left]
-  end
-
   def convert_status(status_id)
     status = [ nil,
       nil,
@@ -152,7 +120,6 @@ module StaticPagesHelper
     user.collections.custom.collect { |c| [ c.custom_name, "#{c.id},#{c.needs_platform}" ] }
   end
 
-
   def check_for_games(options = {})
 
     collections = current_user.collections
@@ -179,26 +146,6 @@ module StaticPagesHelper
   def collection_initial(collection)
     name = collection.custom_name || collection.name
     return name.first.capitalize
-  end
-
-  def random_cover
-    require 'net/https'
-    require 'json'
-    http = Net::HTTP.new('api-v3.igdb.com', 443)
-    http.use_ssl = true
-    request = Net::HTTP::Get.new(URI('https://api-v3.igdb.com/covers'), {'user-key' => 'e9394e9ef83e0c394643dc946d168b15'})
-
-    cover_id = nil
-
-    until cover_id
-      game = rand(15000)
-      request.body = "fields image_id; w game=#{game}; limit 1;"
-      results = JSON.parse http.request(request).body
-      cover_id = results.first
-    end
-
-    cover_url = 'https://images.igdb.com/igdb/image/upload/t_cover_big_2x/'
-    return cover_url += cover_id['image_id'] + ".jpg"
   end
 end
 
