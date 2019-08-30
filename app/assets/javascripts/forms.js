39a23e9861c2d21291ad5bcaffc500540169ff9e
Michal Drawc <pistolisloaded@gmail.com>
2019-08-30 18:15:40 +0200

    Move change search bar placeholder js function form.js

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -81,7 +81,6 @@ function updatePlatformIcon(form_type, id) {
 }
 
 function paintItBlack(id) {
-
   var add_form_link = $('#gs-' + id + ' .plus');
   var game_name = $('#gs-' + id + ' .game-name');
   var other_covers = $('.game-cover').not('#gc-' + id).addClass('bw');
@@ -95,4 +94,23 @@ function paintItBlack(id) {
     game_name.removeClass('disabled');
     other_covers.removeClass('bw');
   });
+}
+
+function changeSearchBar() {
+    $("[id^=search_query_type]").on('input', function() {
+    var selectedVal = "";
+    var selected = $("input[type='radio']:checked");
+    if (selected.length > 0) {
+        selectedVal = selected.val();
+    }
+
+    if (selectedVal === 'char') {
+      $('#search-igdb-bar').attr('placeholder', 'Search video game characters...')
+    } else if (selectedVal === 'dev') {
+      $('#search-igdb-bar').attr('placeholder', 'Search video game developers...')
+    } else if (selectedVal === 'game') {
+      $('#search-igdb-bar').attr('placeholder', 'Search video games...')
+    }
+
+    });
 }
