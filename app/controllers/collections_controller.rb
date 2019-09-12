4b81836c140c9a34b1a28a3f7c2a8c0e902fb764
Michal Drawc <pistolisloaded@gmail.com>
2019-09-12 17:28:32 +0200

    Fix bug: when removing last game form last page redirect to previous page do not work on covers and list view.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,7 +2,7 @@ class CollectionsController < ApplicationController
 before_action :require_user
 before_action :correct_user, only: [:show, :change_view, :edit, :update, :destroy]
 
-PER_PAGE = 20
+PER_PAGE = 2
 
 def new
   @collection = Collection.new
@@ -16,7 +16,7 @@ end
 
 def show
   @q = @collection.games.ransack(params[:q])
-  @view = params[:view] || 'panels'
+  @view = params[:view] || 'list'
   unless params[:q]
     @games = @collection.games.paginate(page: params[:page], per_page: PER_PAGE)
     @refresh = params[:type] == 'refresh'
