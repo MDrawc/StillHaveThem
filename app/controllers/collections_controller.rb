b250606411de56a3ae4d9c2cb1f4d8936e277884
Michal Drawc <pistolisloaded@gmail.com>
2019-09-13 22:57:55 +0200

    Change sort link name 'Date Added' to 'Added'  for better visual alignment. Undo changes to page-links.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -16,7 +16,7 @@ end
 
 def show
   @q = @collection.games.ransack(params[:q])
-  @view = params[:view] || 'list'
+  @view = params[:view] || 'panels'
   unless params[:q]
     @games = @collection.games.paginate(page: params[:page], per_page: PER_PAGE)
     @refresh = params[:type] == 'refresh'
