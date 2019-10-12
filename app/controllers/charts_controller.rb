03e584add7f5421304b4347ed05a544e6cf91a27
Michal Drawc <pistolisloaded@gmail.com>
2019-10-12 18:25:33 +0200

    Add developers chart to single collection charts.

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -7,8 +7,11 @@ class ChartsController < ApplicationController
 
   def graphs
     if params[:collection] == 'all'
+      @data = Collection.data_for_overall_graphs(current_user)
+      @needs_platform = true
     elsif coll = current_user.collections.find_by_id(params[:collection])
       @data = coll.data_for_graphs
+      @needs_platform = coll.needs_platform
     else
       respond_to do |format|
         format.js {render js: 'location.reload();' }
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -7,42 +7,94 @@ class Collection < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :user_id,
     message: "is used by another collection", case_sensitive: false }, length: { maximum: 50 }
 
+  def self.data_for_overall_graphs(user_id)
+      chart1, chart2 = [], []
+      all_games = 0
+      all_physical, all_digital = 0, 0
+      all_platforms = Hash.new(0)
+      all_years = Hash.new(0)
 
-def data_for_graphs
-  if self.needs_platform
+      no_platform = 0
+
+      Collection.where("user_id = ?", user_id).each do |c|
+        games = c.games.pluck(:first_release_date, :platform_name, :physical)
+
+        num_of_games = games.size
+        chart1.append [c.name, num_of_games]
+        all_games += num_of_games
+
+        if c.needs_platform
+          physical = games.count {|g| g[2] == true}
+          all_physical += physical
+          all_digital += num_of_games - physical
+
+          platforms = games.map(&:second)
+          platforms.uniq.each do |p|
+          all_platforms[p] += platforms.count(p)
+          end
+        else
+          no_platform += num_of_games
+        end
+
+        years = games.map { |g| Time.at(g.first).year }
+        years.uniq.each do |y|
+        all_years[y] += years.count(y)
+        end
+      end
+
+      chart2 = [['Physical', all_physical], ['Digital', all_digital]]
+      chart2.append ['No format', all_games - all_physical - all_digital]
+      all_platforms['No platform'] = no_platform
+      chart3 = all_platforms.to_a
+      chart4 = all_years.to_a
+
+      return chart1, chart2, chart3, chart4
+  end
+
+  def data_for_graphs
     games = self.games.pluck(:first_release_date, :platform_name, :physical)
-
-    chart1 = []
-    total = games.size
+    num_of_games = games.size
     user_id = self.user_id
+
+    #Chart 1: Against the rest
     overall = 0
+
     Collection.where("user_id = ?", user_id).each do |c|
       overall += c.games.count
     end
-    chart1.append [self.name, total]
-    chart1.append ['Other collections', overall]
 
-    chart2 = []
-    physical = games.count {|g| g[2] == true}
-    chart2.append ['Physical', physical]
-    chart2.append ['Digital', total - physical]
+    chart1 = [[self.name, num_of_games]]
+    chart1.append ['Other collections', overall - num_of_games]
 
-    chart3 = []
-    platforms = games.map(&:second)
-    platforms.uniq.each do |p|
-      chart3.append [p, platforms.count(p)]
+    if self.needs_platform
+      #Chart 2: Physical games vs digital
+      physical = games.count {|g| g[2] == true}
+      chart2 = [['Physical', physical]]
+      chart2.append ['Digital', num_of_games - physical]
+
+      #Chart 3: Games per platform
+      platforms = games.map(&:second)
+      chart3 = []
+
+      platforms.uniq.each do |p|
+        chart3.append [p, platforms.count(p)]
+      end
+    else
+      char2, chart3 = nil, nil
     end
 
-    chart4 = []
+    #Chart 4: Games per release year
     years = games.map { |g| Time.at(g.first).year }
+    chart4 = []
+
     years.uniq.each do |y|
       chart4.append [y, years.count(y)]
     end
 
-    return chart1, chart2, chart3, chart4
-  else
-
+    #Chart 5: Developers
+    sql = "SELECT developers.name FROM collection_games INNER JOIN developer_games ON collection_games.game_id=developer_games.game_id INNER JOIN developers ON developer_games.developer_id=developers.id WHERE collection_id='#{user_id}';"
+    r = ActiveRecord::Base.connection.execute(sql).values.flatten.sort
+    chart5 = r.inject(Hash.new(0)) {|hash, arr_element| hash[arr_element] += 1; hash }.to_a
+    return chart1, chart2, chart3, chart4, chart5
   end
-end
-
 end
