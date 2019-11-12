0aa8f53364bed4dec54b932d6828f87657db9af9
Michal Drawc <pistolisloaded@gmail.com>
2019-11-12 18:30:57 +0100

    Finish delete account option in users settings.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
 
   has_many :collections, dependent: :destroy
-  has_many :user_platforms
+  has_many :user_platforms, dependent: :destroy
   has_many :platforms, through: :user_platforms
   has_many :records, dependent: :destroy
   has_many :shares, dependent: :destroy
