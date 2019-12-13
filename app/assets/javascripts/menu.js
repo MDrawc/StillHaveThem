5cb0af7f4647ddfcd5a2fec9272ab7a49d680a96
Michal Drawc <pistolisloaded@gmail.com>
2019-12-13 21:15:13 +0100

    Delete custom_data_confirm function and calls

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -4,42 +4,6 @@ function remove_active_create_coll() {
     });
 }
 
-function custom_data_confirm() {
-
-    var handleConfirm = function(element) {
-        if (!allowAction(this)) {
-            Rails.stopEverything(element)
-        }
-    }
-    var allowAction = function(element) {
-        if (element.getAttribute('data-confirm') === null) {
-            return true
-        }
-        showConfirmDialog(element);
-        return false
-    }
-    var confirmed = function(element, result) {
-        if (result.value) {
-            element.removeAttribute('data-confirm')
-            element.setAttribute('data-remote', true)
-            element.click()
-        }
-        return false
-    }
-
-    var showConfirmDialog = function(link) {
-        var message = $(link).attr('data-confirm');
-        UIkit.modal.confirm(message).then(function() {
-            confirmed(link, {
-                value: true
-            });
-        });
-        $('.uk-button-primary').html('<span uk-icon="trash"></span>')
-    }
-
-    $("a[data-confirm]").on('click', handleConfirm);
-}
-
 function hideShowBackToTop() {
     var $button = $('#scroll-up');
     $(window).scroll(function() {
@@ -50,7 +14,3 @@ function hideShowBackToTop() {
         }
     });
 }
-
-$(function(){
-  custom_data_confirm();
-});
