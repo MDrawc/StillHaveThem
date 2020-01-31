457debe2482762d71946d276eaaffcc4031cf064
Michal Drawc <pistolisloaded@gmail.com>
2020-01-31 16:25:21 +0100

    Finish remember me feature.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in(user)
+      log_in user
+      params[:session][:rem_me] == '1' ? remember(user) : forget(user)
       reload
     else
       respond_to do |format|
