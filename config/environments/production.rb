007a33c468e066e37aefe2f17e6dc9c47cfef07b
Michal Drawc <pistolisloaded@gmail.com>
2020-02-03 14:12:05 +0100

    Change host in mailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'yvgc.herokuapp.com'
+  host = 'stillhavethem.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
