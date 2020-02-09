aec204065f827c23c97d3805e508787a16bc098b
Michal Drawc <pistolisloaded@gmail.com>
2020-02-09 18:11:57 +0100

    Finish refactoring search igdb controller.

--- app/controllers/search_igdb_controller.rb
+++ app/controllers/search_igdb_controller.rb
@@ -12,56 +12,36 @@ class SearchIgdbController < ApplicationController
         CreateHistoryRecord.call(user: current_user,
                                  search_params: search_params,
                                  results_size: results_size)
-        unless results_size.zero?
-          @results = @inquiry.results
-          @@last_result_ids = @results.pluck(:igdb_id)
-          @owned = FindOwnedGames.call(user: current_user, results: @results)
-        end
+        prepare_results(is_load_more: false) unless results_size.zero?
         @history_records = current_user.records
         js_partial('search', { view: view })
       else
         js_partial('error', { message: @inquiry.error_msg })
       end
-    elsif params[:last_form]
-
-
-
-      last_form = JSON.parse params[:last_form].gsub('=>', ':') unless params[:last_form].empty?
-      last_query = JSON.parse params[:last_query].gsub('=>', ':')
-
+    elsif (last_form = params[:last_form])
+      last_form = string_to_hash(last_form) unless last_form.empty?
+      last_query = string_to_hash(params[:last_query])
       new_offset = params[:last_offset].to_i + SearchIgdb::RESULT_LIMIT
       @inquiry = SearchIgdb.new(last_form, new_offset, last_query)
-
-
-
-
-
-
       @inquiry.search
-
       @inquiry.fix_duplicates(@@last_result_ids) if @inquiry.query_type == :game
-
-      if @inquiry.results.present?
-        @results = @inquiry.results
-        @@last_result_ids += @results.pluck(:igdb_id)
-        @owned = FindOwnedGames.call(user: current_user, results: @results)
-      end
-
-
-
-
-
+      prepare_results(is_load_more: true) unless @inquiry.results.empty?
       js_partial('load_more', { view: view })
     end
   end
 
   private
-  def first_search
+  def prepare_results(is_load_more:)
+    @results = @inquiry.results
+    unless is_load_more
+      @@last_result_ids = @results.pluck(:igdb_id)
+    else
+      @@last_result_ids += @results.pluck(:igdb_id)
+    end
+    @owned = FindOwnedGames.call(user: current_user, results: @results)
   end
 
-  def load_more
-  end
-
-  def prepare_results()
+  def string_to_hash(data)
+    JSON.parse data.gsub('=>', ':')
   end
 end
