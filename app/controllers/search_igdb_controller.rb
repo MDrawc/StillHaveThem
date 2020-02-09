85623532277ad2ce7d619ceec6725a0b97d02618
Michal Drawc <pistolisloaded@gmail.com>
2020-02-09 12:55:10 +0100

    Add two new service objects: CreateHistoryRecord and FindOwnedGames

--- app/controllers/search_igdb_controller.rb
+++ app/controllers/search_igdb_controller.rb
@@ -4,37 +4,24 @@ class SearchIgdbController < ApplicationController
 
   def search
     view = params[:view] || cookies['s_view'] || 'cover_view'
-    if params[:search]
-      @inquiry = SearchIgdb.new(params[:search])
+    if (search_params = params[:search])
+      @inquiry = SearchIgdb.new(search_params)
       if @inquiry.validate!
-
-
-
         @inquiry.search
-        search_record = build_record(params[:search])
-        search_record.results = @inquiry.results.size
-        search_record.save
-
-
-
-        if @inquiry.results.present?
-          #Prepare data to fix_duplicates with new offset request:
-          @@last_result_ids = @inquiry.results.map { |game| game[:igdb_id] }
-
-
-          #Check owned games for underlining them in view:
+        results_size = @inquiry.results.size
+        CreateHistoryRecord.call(user: current_user,
+                                 search_params: search_params,
+                                 results_size: results_size)
+        unless results_size.zero?
           @results = @inquiry.results
-          @owned = owned(@results)
+          @@last_result_ids = @results.map { |game| game[:igdb_id] }
+          @owned = FindOwnedGames.call(user: current_user, results: @results)
         end
-
         @history_records = current_user.records
         js_partial('search', { view: view })
       else
         js_partial('error', { message: @inquiry.error_msg })
       end
-
-
-
     elsif params[:last_form]
 
 
@@ -45,41 +32,24 @@ class SearchIgdbController < ApplicationController
                  eval(params[:last_query]))
 
       @inquiry.search
-      #Fixing duplicates
       @inquiry.fix_duplicates(@@last_result_ids) if @inquiry.query_type == :game
 
       if @inquiry.results.present?
-        #Prepare data to fix_duplicates with new offset request:
         @@last_result_ids += @inquiry.results.map { |game| game[:igdb_id] }
-        #Check owned games for underlining them in view:
         @owned = owned(@inquiry.results)
         @results = @inquiry.results
       end
-
-
-
       js_partial('load_more', { view: view })
     end
   end
 
   private
-    def owned(results)
-      res = []
-      igdb_ids = results.map { |g| g[:igdb_id] }
-      current_user.collections.includes(:games).each do |c|
-        res += c.games.where(igdb_id: igdb_ids).pluck(:igdb_id)
-      end
-      return res.uniq
-    end
+  def first_search
+  end
 
-    def build_record(s_par)
-      f_values = s_par.values[2..]
+  def load_more
+  end
 
-      data = { inquiry: s_par[:inquiry].strip.downcase,
-       query_type: s_par[:query_type],
-       filters: f_values,
-       custom_filters: !f_values.map {|f| f == '1'}.all? }
-
-      record = current_user.records.build(data)
-    end
+  def prepare_results
+  end
 end
---
+++ app/services/create_history_record.rb
@@ -0,0 +1,28 @@
+class CreateHistoryRecord < ApplicationService
+  def initialize(user:, search_params:, results_size:)
+    @user = user
+    @search_params = search_params
+    @results_size = results_size
+  end
+
+  def call
+    build_record
+    save_record
+  end
+
+  private
+
+  def build_record
+    filter_values = @search_params.values[2..]
+    data = { inquiry: @search_params[:inquiry].strip.downcase,
+     query_type: @search_params[:query_type],
+     filters: filter_values,
+     custom_filters: !filter_values.map {|f| f == '1'}.all?,
+     results: @results_size }
+    @record = @user.records.build(data)
+  end
+
+  def save_record
+    @record.save
+  end
+end
